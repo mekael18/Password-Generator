5d0214ae0ab0e29bca59f8972c820336e564abb3
Mekael <mekael13.me@gmail.com>
2023-02-16 21:09:40 -0500

    fixed code

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,57 @@
 // Assignment Code
 function generatePassword() {
-  var password = "";
-  var passwordLength = prompt("How many characters would you like your password to be? (Must be between 8 and 128 characters)");
-  var passwordLength = parseInt(passwordLength);
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert("Password must be between 8 and 128 characters");
-    return;
-  }
-  var passwordLowercase = confirm("Would you like to include lowercase letters?");
-  var passwordUppercase = confirm("Would you like to include uppercase letters?");
-  var passwordNumbers = confirm("Would you like to include numbers?");
-  var passwordSpecialCharacters = confirm("Would you like to include special characters?");
-  var passwordCharacters = "";
-  if (passwordLowercase) {
-    passwordCharacters += "abcdefghijklmnopqrstuvwxyz";
-  }
-  if (passwordUppercase) {
-    passwordCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  }
-  if (passwordNumbers) {
-    passwordCharacters += "0123456789";
-  }
-  if (passwordSpecialCharacters) {
-    passwordCharacters += "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
-  }
-  if (passwordCharacters === "") {
-    alert("You must select at least one character type");
-    return;
-  }
-  for (var i = 0; i < passwordLength; i++) {
-    password += passwordCharacters.charAt(Math.floor(Math.random() * passwordCharacters.length));
-  }
-  return password;
+var passwordLength = prompt("How many characters would you like your password to contain?");
+passwordLength = parseInt(passwordLength);
+
+while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+  alert("Password length must be between 8 and 128 characters. Try again.");
+  passwordLength = prompt("How many characters would you like your password to contain?");
+  passwordLength = parseInt(passwordLength);
 }
+
+var passwordLowercase = confirm("Click OK to confirm including lowercase characters.");
+var passwordUppercase = confirm("Click OK to confirm including uppercase characters.");
+var passwordNumeric = confirm("Click OK to confirm including numeric characters.");
+var passwordSpecial = confirm("Click OK to confirm including special characters.");
+
+while (!passwordLowercase && !passwordUppercase && !passwordNumeric && !passwordSpecial) {
+  alert("You must select at least one character type. Try again.");
+  passwordLowercase = confirm("Click OK to confirm including lowercase characters.");
+  passwordUppercase = confirm("Click OK to confirm including uppercase characters.");
+  passwordNumeric = confirm("Click OK to confirm including numeric characters.");
+  passwordSpecial = confirm("Click OK to confirm including special characters.");
+}
+
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "0123456789";
+var special = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+var password = "";
+var possibleCharacters = "";
+
+if (passwordLowercase) {
+  possibleCharacters += lowercase;
+}
+
+if (passwordUppercase) {
+  possibleCharacters += uppercase;
+}
+
+if (passwordNumeric) {
+  possibleCharacters += numeric;
+}
+
+if (passwordSpecial) {
+  possibleCharacters += special;
+}
+
+for (var i = 0; i < passwordLength; i++) {
+  password += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+}
+
+return password;
+}
+
 
 var generateBtn = document.querySelector("#generate");
 
